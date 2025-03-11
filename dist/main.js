798a07076c223f53106b6ec0bd433bf0538ee4fd
matheusfredo <matheusfredo16@outlook.com>
2025-03-11 18:45:46 -0300

    Implementing the style guide; creating the notification strip and navbar

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,6 @@
 "use strict";
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
---
+++ scripts/main.ts
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById("menu-toggle") as HTMLButtonElement;
+const navLinks = document.querySelector(".nav-links") as HTMLElement;
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
